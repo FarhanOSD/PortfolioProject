9911ec67796916c50eff76ee1b0817ee6e06c1f5
FarhanOSD <ff3841362@gmail.com>
2025-02-21 18:05:37 +0600

    #

--- src/comone/index.js
+++ src/comone/index.js
@@ -1,11 +1,11 @@
-import project1 from '../assets/project-1.jpg';
-import project2 from '../assets/project-2.jpg';
-import project3 from '../assets/project-3.jpg';
-import project4 from '../assets/project-4.jpg';
+import project1 from '../assets/project-1.png';
+import project2 from '../assets/project-2.png';
+import project3 from '../assets/project-3.png';
+import project4 from '../assets/project-4.png';
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
@@ -44,33 +44,55 @@ export const PROJECTS = [
     image: project1,
     description:
       'A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.',
-    technologies: ['HTML', 'CSS', 'React', 'Node.js', 'MongoDB'],
+    technologies: [
+      'NodeJs',
+      'Tailwind CSS',
+      'React',
+      'ExpressJs',
+      'MongoDB',
+      'JWT',
+    ],
   },
   {
-    title: 'Task Management App',
+    title: 'Authentication and Authorisation Project',
     image: project2,
     description:
-      'An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.',
-    technologies: ['HTML', 'CSS', 'Angular', 'Firebase'],
+      'An Authentication Project for managing security and avoid vulnerability, with features such as mailing codes to authorize real user.',
+    technologies: [
+      'NodeJs',
+      'Tailwind CSS',
+      'ReactJs',
+      'Mailtrap',
+      'Framer-Motion',
+      'ExpressJs',
+     
+    ],
   },
   {
     title: 'Portfolio Website',
     image: project3,
     description:
       'A personal portfolio website showcasing projects, skills, and contact information.',
-    technologies: ['HTML', 'CSS', 'React', 'Bootstrap'],
+    technologies: ['HTML', 'Tailwind CSS', 'React', 'React-Icons'],
   },
   {
-    title: 'Blogging Platform',
+    title: 'Movie Search app',
     image: project4,
     description:
-      'A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.',
-    technologies: ['HTML', 'CSS', 'Vue.js', 'Express', 'mySQL'],
+      'A platform for creating and searching movie list, with features like Searching best movies.',
+    technologies: [
+      'HTML',
+      'Vue.js',
+      'HTML',
+      'Tailwind CSS',
+      'React',
+      'React-Icons',
+    ],
   },
 ];
 
 export const CONTACT = {
-  address: '767 Fifth Avenue, New York, NY 10153 ',
-  phoneNo: '+12 4555 666 00 ',
-  email: 'me@example.com',
+  address: 'Salna, Gazipur, Dhaka Bangladesh ',
+  phoneNo: '+88 01540086864 ',
+  email: 'farhan,ahmed.tamkin@gmail.com',
 };
