71d4871f5f781880ab4ccd0d5c3c3563b4d38029
FARKANAHMEDTAHAKIN <ftahakin@gmail.com>
2025-02-26 11:18:56 +0600

    #

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -45,7 +45,7 @@ function Hero() {
             </motion.p>
           </div>
         </div>
-        <div className="w-full lg:w-1/2 lg:p-8 ">
+        <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center ">
             <motion.img
               initial={{ x: 100, opacity: 0 }}
